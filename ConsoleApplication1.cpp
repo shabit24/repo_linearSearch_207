42888db2af677d4d31b385cb73ee3803e5cf170d
shabit24 <hossain.shabit24@gmail.com>
2023-04-14 10:48:10 +0700

    Number of comparison

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -3,4 +3,27 @@
 using namespace std;
 
 int arr[20];
+int n; // Numberof element
+int i; // Index of array
 
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of array: ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and  maximum 20\n\n";
+	}
+
+	//Accept array element
+	cout << "\n------------------\n";
+	cout << "Enter array element";
+
+}
+void LinearSearch() {
+	char ch;
+	int comparison; //Number of comparison
+}
