4cda53d389f36d4665e0f607e3ac666415d2d1c5
shabit24 <hossain.shabit24@gmail.com>
2023-04-14 11:11:18 +0700

    check condition

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -37,21 +37,21 @@ void LinearSearch() {
 		comparison = 0;
 		for (i = 0;i < n;i++)
 		{
-			comparison++
-				if (arr[i] == item)
+			comparison++;
+				if (arr[i] == item)    //condition check
 				{
-					cout << "\n" << item << "found at position" << (i + 1) << end;
+					cout << "\n" << item << "found at position" << (i + 1) << endl;
 					break;
 				}
 		}
 
 		if (i == n)
 			cout << "\n" << item << "not found  in the array";
-		cout << "\nNumber of comparison: " << comparison << end;
+		cout << "\nNumber of comparison: " << comparison << endl;
 		cout << "\ncountinu search (y/n): ";
 		cin >> ch;
 
-	} while ((ch == 'y') || (ch == 'Y');
+	} while ((ch == 'y') || (ch == 'Y'));
 
 }
 
